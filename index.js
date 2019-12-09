fc97cf542a8ce160b334056eabb4d3f26ad69536
amir.duka <a.duka@outlook.com>
2019-12-09 16:49:51 -0600

    add HTML and CSS file server running on cloud

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express()
 const port = 3000
 const port = process.env.PORT || 3000
-
+// to use pug
 app.set('view engine', 'pug');
+
 app.use(express.static("public"));
 
 app.get('', (req, res) => {
