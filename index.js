cebae51d51f32231310d3c9cd6a07b2157cd9285
amir.duka <a.duka@outlook.com>
2019-12-09 18:06:28 -0600

    modified index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 3000
+// const port = 3000
 const port = process.env.PORT || 3000
 // to use pug
 app.set('view engine', 'pug');
